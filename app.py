1e396c1930ab56689ca3721d4672248985aaab26
何宇峰 <supermonk00@heyufengs-MacBook-Pro.local>
2021-11-11 16:33:27 +0800

    have to success

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import os
 from flask import Flask
-
-
-posts = article_list.posts
-
 app = Flask(__name__)
+ 
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def index():
+  return '<h1>Hello World</h1>'
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
