c968279303901d81a66de162b5eb7b25861606ca
何宇峰 <supermonk00@heyufengs-MacBook-Pro.local>
2021-11-13 02:45:59 +0800

    make it better

--- mario.py
+++ mario.py
@@ -4,4 +4,6 @@
 def index():
   return "<h1>Mario is my Spanish teacher </h1>"
 
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
